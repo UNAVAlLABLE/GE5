6cf84c48f7a94546f7ab433dc303452bf48a54f2
UNAVAlLABLE <hashimnader@gmail.com>
2014-06-29 09:55:16 -0400

    Added Vector pos and render method to Entity class

--- src/ge5/Entity.java
+++ src/ge5/Entity.java
@@ -1,15 +1,22 @@
 package ge5;
 
+import java.awt.Graphics2D;
+
 public class Entity {
 	
 	static int IDCounter = 0;
 	
 	public int ID;
+	public Vector pos;
 	
-	Entity(){
+	protected Entity(){
 		
 		ID = IDCounter++;
 			
+	}
+	
+	protected void render(Graphics2D g){
+		
 	}
 
 }
