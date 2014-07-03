7a4e0c8704a8e954718e20b15e15ea2ce983ead7
Said6289 <alattrach.said@yahoo.com>
2014-07-03 05:28:57 +0400

    Just a first test with blend maps Doesn't blend properly yet

--- src/ge5/MapManager.java
+++ src/ge5/MapManager.java
@@ -4,9 +4,8 @@
 
 package ge5;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
-//import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImage;
 
 public class MapManager {
 	
@@ -14,49 +13,47 @@ public class MapManager {
 	private TextureManager textureManager;
 	
 	private Tile[][] tiles;
+	private final int tileSize;
 	
 	private int width;
 	private int height;
 	
-	private float halfWidth;
-	private float halfHeight;
+	private int halfWidth;
+	private int halfHeight;
 	
-	private int tileWidth;
-	private int tileHeight;
-	
-//	private BufferedImage img;
-	
+	private BufferedImage map;
+		
 	protected MapManager(Camera camera) {
 		
 		this.camera = camera;
-		camera.setMap(this);
 		
-		textureManager = new TextureManager(2);
+		textureManager = new TextureManager(2, 1);
 		textureManager.loadTexture("grass.jpg", 0);
+		textureManager.loadTexture("sand.jpg", 1);
+		textureManager.loadBlendMap("bt1.bmp", 0);
+		
+		tileSize = textureManager.textureSize;
 		
 		width = 3;
 		height = 3;
-		halfWidth = width / 2.0f;
-		halfHeight = height / 2.0f;
+		halfWidth = width / 2;
+		halfHeight = height / 2;
 		
 		tiles = new Tile[width][height];
 		
 		tiles[0][0] = new Tile(0);
-		tiles[0][1] = new Tile(0);
+		tiles[0][1] = new Tile(1);
 		tiles[0][2] = new Tile(0);
-		tiles[1][0] = new Tile(0);
+		tiles[1][0] = new Tile(1);
 		tiles[1][1] = new Tile(0);
-		tiles[1][2] = new Tile(0);
+		tiles[1][2] = new Tile(1);
 		tiles[2][0] = new Tile(0);
-		tiles[2][1] = new Tile(0);
+		tiles[2][1] = new Tile(1);
 		tiles[2][2] = new Tile(0);
 		
-		setTileDimensions();
-		
-//		img = new BufferedImage(tileWidth * width, tileHeight * height, BufferedImage.TYPE_INT_ARGB);
-//		
-//		img.setRGB(0, 0, width * tileWidth, height * tileHeight, interpolate(), 0, width * tileWidth);
-		
+		map = new BufferedImage(width * tileSize, height * tileSize, BufferedImage.TYPE_INT_RGB);
+		blendTiles();
+				
 	}
 	
 	protected void render(Graphics2D g) {
@@ -65,12 +62,7 @@ public class MapManager {
 			
 			for (int j = 0; j < tiles[0].length; j++) {
 				
-//				g.drawImage(img, null, 0, 0);
-				
-				int x = (int) (tileWidth * (i - halfWidth) + camera.halfScreenWidth);
-				int y = (int) (tileHeight * (j - halfHeight) + camera.halfScreenHeight);
-				
-				g.drawImage(textureManager.textures[tiles[i][j].textureID], null, x, y);
+				g.drawImage(map, null, camera.halfScreenWidth - map.getWidth() / 2, camera.halfScreenHeight - map.getHeight() / 2);
 			
 			}
 			
@@ -78,247 +70,52 @@ public class MapManager {
 	
 	}
 	
-	protected void setTileDimensions() {
-				
-		float height = camera.getHeight();
+	// Doesn't do exactly what we want yet
+	// Only works if blend map has exact same resolution as texture
+	private void blendTiles() {
 		
-		tileWidth = (int) (camera.screenWidth / (camera.ratio * height));
-		tileHeight = (int) (camera.screenHeight / height);
-	
+		Graphics2D g = map.createGraphics();
+		
+		int blendMapSize = textureManager.blendMapSize;
+		
+		int pixels[] = map.getRGB(0, 0, map.getWidth(), map.getHeight(), null, 0, map.getWidth());
+		int blendMap[] = textureManager.blendMaps[0];
+		
+		for (int i = 0; i < tiles.length; i++) {
+			
+			for (int j = 0; j < tiles[0].length; j++) {
+			
+				int texture[] = textureManager.textures[tiles[i][j].textureID];
+				
+				int startX = i * tileSize - (blendMapSize - tileSize) / 2;
+				int startY = j * tileSize - (blendMapSize - tileSize) / 2;
+				
+				for (int x = 0; x < blendMapSize; x++) {
+					
+					for (int y = 0; y < blendMapSize; y++) {
+						
+						int index = startX + x + (startY + y) * map.getWidth();
+						
+						if (blendMap[x + y * blendMapSize] == 0) {
+							
+							pixels[index] = texture[x + y * blendMapSize];
+							
+						}
+						
+					}
+					
+				}
+			
+			}
+		
+		}
+		
+		map.setRGB(0, 0, map.getWidth(), map.getHeight(), pixels, 0, map.getWidth());
+				
+		g.drawImage(map, null, 0, 0);
+		
+		g.dispose();
+		
 	}
-	
-//	private int[] interpolate() {
-//		
-//		int[] pixels = new int[width * height * tileWidth * tileHeight];
-//		
-//		int localTileCenterX = tileWidth / 2;
-//		int localTileCenterY = tileHeight / 2;
-//		
-//		int pixelX, pixelY;
-//		int x1, x2, y1, y2;
-//		int up, down, left, right;
-//		int Q11, Q21, Q12, Q22;
-//		
-//		int i, j, x, y;
-//		
-//		for (i = 0; i < tiles.length; i++) {
-//						
-//			for (j = 0; j < tiles[0].length; j++) {
-//								
-//				if (i == 0)
-//					left = i;
-//				else
-//					left = i - 1;
-//				
-//				if (j == 0)
-//					up = j;
-//				else
-//					up = j - 1;
-//				
-//				right = i;
-//				down = j;
-//				
-//				x1 = left * tileWidth + localTileCenterX;
-//				y1 = up * tileHeight + localTileCenterY;
-//				
-//				x2 = right * tileWidth + localTileCenterX;
-//				y2 = down * tileHeight + localTileCenterY;
-//				
-//				Q11 = tiles[left][up].color;
-//				Q21 = tiles[right][up].color;
-//				Q12 = tiles[left][down].color;
-//				Q22 = tiles[right][down].color;
-//								
-//				for (x = 0; x < localTileCenterX; x++) {
-//					
-//					for (y = 0; y < localTileCenterY; y++) {
-//						
-//						pixelX = i * tileWidth + x;
-//						pixelY = j * tileHeight + y;
-//						
-//						pixels[pixelX + pixelY * tileWidth * width] = (int) interpolateColor(pixelX, pixelY, x1, y1, x2, y2, Q11, Q21, Q12, Q22);
-//						
-//					}
-//					
-//				}
-//				
-//				if (i == 0)
-//					left = i;
-//				else
-//					left = i - 1;
-//				
-//				if (j == tiles[0].length - 1)
-//					down = j;
-//				else
-//					down = j + 1;
-//				
-//				right = i;
-//				up = j;
-//				
-//				x1 = left * tileWidth + localTileCenterX;
-//				y1 = up * tileHeight + localTileCenterY;
-//				
-//				x2 = right * tileWidth + localTileCenterX;
-//				y2 = down * tileHeight + localTileCenterY;
-//				
-//				Q11 = tiles[left][up].color;
-//				Q21 = tiles[right][up].color;
-//				Q12 = tiles[left][down].color;
-//				Q22 = tiles[right][down].color;
-//				
-//				for (x = 0; x < localTileCenterX; x++) {
-//					
-//					for (y = localTileCenterY; y < tileHeight; y++) {
-//												
-//						pixelX = i * tileWidth + x;
-//						pixelY = j * tileHeight + y;
-//						
-//						pixels[pixelX + pixelY * tileWidth * width] = (int) interpolateColor(pixelX, pixelY, x1, y1, x2, y2, Q11, Q21, Q12, Q22);
-//						
-//					}
-//					
-//				}
-//				
-//				if (i == tiles.length - 1)
-//					right = i;
-//				else
-//					right = i + 1;
-//				
-//				if (j == 0)
-//					up = j;
-//				else
-//					up = j - 1;
-//				
-//				left = i;
-//				down = j;
-//				
-//				x1 = left * tileWidth + localTileCenterX;
-//				y1 = up * tileHeight + localTileCenterY;
-//				
-//				x2 = right * tileWidth + localTileCenterX;
-//				y2 = down * tileHeight + localTileCenterY;
-//				
-//				Q11 = tiles[left][up].color;
-//				Q21 = tiles[right][up].color;
-//				Q12 = tiles[left][down].color;
-//				Q22 = tiles[right][down].color;
-//				
-//				for (x = localTileCenterX; x < tileWidth; x++) {
-//					
-//					for (y = 0; y < localTileCenterY; y++) {
-//						
-//						pixelX = i * tileWidth + x;
-//						pixelY = j * tileHeight + y;
-//						
-//						pixels[pixelX + pixelY * tileWidth * width] = (int) interpolateColor(pixelX, pixelY, x1, y1, x2, y2, Q11, Q21, Q12, Q22);
-//						
-//					}
-//				
-//				}
-//					
-//				if (i == tiles.length - 1)
-//					right = i;
-//				else
-//					right = i + 1;
-//				
-//				if (j == tiles[0].length - 1)
-//					down = j;
-//				else
-//					down = j + 1;
-//				
-//				left = i;
-//				down = j;
-//				
-//				x1 = left * tileWidth + localTileCenterX;
-//				y1 = up * tileHeight + localTileCenterY;
-//				
-//				x2 = right * tileWidth + localTileCenterX;
-//				y2 = down * tileHeight + localTileCenterY;
-//				
-//				Q11 = tiles[left][up].color;
-//				Q21 = tiles[right][up].color;
-//				Q12 = tiles[left][down].color;
-//				Q22 = tiles[right][down].color;
-//				
-//				for (x = localTileCenterX; x < tileWidth; x++) {
-//				
-//					for (y = localTileCenterY; y < tileHeight; y++) {
-//						
-//						pixelX = i * tileWidth + x;
-//						pixelY = j * tileHeight + y;
-//						
-//						pixels[pixelX + pixelY * tileWidth * width] = (int) interpolateColor(pixelX, pixelY, x1, y1, x2, y2, Q11, Q21, Q12, Q22);
-//						
-//					}
-//					
-//				}
-//				
-//			}
-//			
-//		}
-//		
-//		return pixels;
-//		
-//	}
-//	
-//	private float interpolateColor(int x, int y, int x1, int y1, int x2, int y2, int Q11, int Q21, int Q12, int Q22) {
-//		
-//		int R11 = (Q11 & 0x00FF0000) >> 16;
-//		int R21 = (Q21 & 0x00FF0000) >> 16;
-//		int R12 = (Q12 & 0x00FF0000) >> 16;
-//		int R22 = (Q22 & 0x00FF0000) >> 16;
-//					
-//		int r = (int) interpolateBilinear(x, y, x1, x2, y1, y2, R11, R21, R12, R22);
-//		
-//		int G11 = (Q11 & 0x0000FF00) >> 8;
-//		int G21 = (Q21 & 0x0000FF00) >> 8;
-//		int G12 = (Q12 & 0x0000FF00) >> 8;
-//		int G22 = (Q22 & 0x0000FF00) >> 8;
-//					
-//		int g = (int) interpolateBilinear(x, y, x1, x2, y1, y2, G11, G21, G12, G22);
-//		
-//		int B11 = (Q11 & 0x000000FF);
-//		int B21 = (Q21 & 0x000000FF);
-//		int B12 = (Q12 & 0x000000FF);
-//		int B22 = (Q22 & 0x000000FF);
-//					
-//		int b = (int) interpolateBilinear(x, y, x1, x2, y1, y2, B11, B21, B12, B22);
-//						
-//		return (r << 16) | (g << 8) | b | 0xFF000000;
-//		
-//	}
-//	
-//	private float interpolateBilinear(int x, int y, int x1, int y1, int x2, int y2, int Q11, int Q21, int Q12, int Q22) {
-//				
-//		if (x1 == x2 && y1== y2)
-//			return 0;
-//
-//		if (x1 == x2) {
-//			return 0;
-//			//return Q11 + (Q12 - Q11) * (y - y1) / (y2 - y1);
-//		}
-//	
-//		if (y1 == y2) {
-//			return 0;
-//			//return Q12 + (Q22 - Q12) * (x - x1) / (x2 - x1);
-//		}
-//		int divisor = (x2 - x1) * (y2 - y1);
-//		
-//		int deltaX1 = x - x1;
-//		int deltaY1 = y - y1;
-//		
-//		int deltaX2 = x2 - x;
-//		int deltaY2 = y2 - y;
-//		
-//		float newColor = Q11 * deltaX2 * deltaY2;
-//		newColor += Q21 * deltaX1 * deltaY2;
-//		newColor += Q12 * deltaX2 * deltaY1;
-//		newColor += Q22 * deltaX1 * deltaY1;
-//				
-//		newColor /= divisor;
-//				
-//		return newColor;
-//		
-//	}
 	
 }
--- src/ge5/TextureManager.java
+++ src/ge5/TextureManager.java
@@ -1,17 +1,25 @@
 package ge5;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
 
+// Should probably rename this to something else
 public class TextureManager {
 	
-	protected BufferedImage[] textures;
+	// Just for now...
+	protected final int textureSize = 64;
+	protected final int blendMapSize = 64;
 	
-	protected TextureManager(int numTextures) {
+	protected int[][] textures;
+	protected int[][] blendMaps;
+	
+	protected TextureManager(int numTextures, int numBlendMaps) {
 		
-		textures = new BufferedImage[numTextures];
+		textures = new int[numTextures][textureSize];
+		blendMaps = new int[numBlendMaps][blendMapSize];
 		
 	}
 	
@@ -19,11 +27,64 @@ public class TextureManager {
 		
 		try {
 			
-			textures[index] = ImageIO.read(new File("res/" + fileName));
-		
+			BufferedImage img = ImageIO.read(new File("res/" + fileName));
+			
+			// Resize if necessary
+			if (img.getWidth() != textureSize || img.getHeight() != textureSize) {
+				
+				BufferedImage newImg = new BufferedImage(textureSize, textureSize, BufferedImage.TYPE_INT_RGB);
+				Graphics2D g = newImg.createGraphics();
+				g.drawImage(img, 0, 0, textureSize, textureSize, null);
+				g.dispose();
+				
+				img = newImg;
+				
+			}
+			
+			textures[index] = img.getRGB(0, 0, textureSize, textureSize, null, 0, textureSize);
+			
 		} catch (Exception e) {
 			
-			System.out.println("Failed to load image file");
+			System.out.println("Failed to load texture");
+			
+		}
+		
+	}
+	
+	protected void loadBlendMap(String fileName, int index) {
+		
+		try {
+			
+			BufferedImage img = ImageIO.read(new File("res/" + fileName));
+			
+			// Resize if necessary
+			if (img.getWidth() != blendMapSize || img.getHeight() != blendMapSize) {
+				
+				BufferedImage newImg = new BufferedImage(blendMapSize, blendMapSize, BufferedImage.TYPE_INT_RGB);
+				Graphics2D g = newImg.createGraphics();
+				g.drawImage(img, 0, 0, blendMapSize, blendMapSize, null);
+				g.dispose();
+				
+				img = newImg;
+				
+			}
+			
+			int[] blendMap = new int[img.getWidth() * img.getHeight()];
+			int[] pixels = img.getRGB(0, 0, blendMapSize, blendMapSize, null, 0, blendMapSize);
+			
+			// For easier checks
+			for (int i = 0; i < pixels.length; i++) {
+				if ((pixels[i] & 0x000000FF) == 0)
+					blendMap[i] = 0;
+				else
+					blendMap[i] = 1;
+			}
+			
+			blendMaps[index] = blendMap;
+			
+		} catch (Exception e) {
+			
+			System.out.println("Failed to load blend map");
 			
 		}
 		
